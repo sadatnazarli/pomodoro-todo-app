48c8696c1c104346c01b6cd19dfa8091cdfd9116
Sadat <118483002+sadatnazarli@users.noreply.github.com>
2023-11-10 16:47:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function startPomodoro() {
+    // ... existing code
+
+    // Add 'active' class to start the animation
+    document.getElementById('pomodoro-timer').classList.add('active');
+}
+
+function stopPomodoro() {
+    // ... existing code
+
+    // Remove 'active' class to stop the animation
+    document.getElementById('pomodoro-timer').classList.remove('active');
+}
